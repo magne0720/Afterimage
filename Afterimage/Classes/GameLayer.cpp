8d1f26f2b30f769beb879ed25e1fb6914199bd55
野津　正太郎 <nkc.syoutarou.nozu@gmail.com>
2016-07-19 14:24:24 +0900

    ステージの地面の画像

--- Afterimage/Classes/GameLayer.cpp
+++ Afterimage/Classes/GameLayer.cpp
@@ -9,6 +9,14 @@ bool GameLayer::init()
 	player = Player::create();
 	this->addChild(player, 2);
 
+	for (int i = 0; i < 11;i++)
+	{
+		Sprite* s = Sprite::create("tile.png");
+		s->setAnchorPoint(Vec2::ANCHOR_BOTTOM_LEFT);
+		s->setPosition(Vec2( i * 200, 0));
+		addChild(s);
+	}
+
 
 
 	return true;
