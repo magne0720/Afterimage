09ec018dd28e264b359a72410c2fc356e48fd5dd
野津　正太郎 <nkc.syoutarou.nozu@gmail.com>
2016-09-29 20:08:23 +0900

    説明文作ったったｗｗｗｗｗｗｗ

--- Afterimage/Classes/StageLayer.cpp
+++ Afterimage/Classes/StageLayer.cpp
@@ -1,5 +1,13 @@
+ï»¿#pragma execution_character_set("utf-8")
+
 #include "StageLayer.h"
 #include "GameScene.h"
+
+#define ALL_STAGE 30
+#define XX designResolutionSize.width*0.7f
+#define YY designResolutionSize.height*0.1f
+#define PX designResolutionSize.width*0.7f
+#define PY designResolutionSize.height*2.5f
 
 bool StageLayer::init()
 {
@@ -7,6 +15,12 @@ bool StageLayer::init()
 	{
 		return false;
 	}
+	StageNumber = 0;
+	moveSpeed = 0;
+	numbersEye =1;
+	RX = 0;
+	RY = 0;
+
 	auto tap = EventListenerTouchOneByOne::create();
 	tap->setSwallowTouches(true);
 	tap->onTouchBegan = CC_CALLBACK_2(StageLayer::onTouchBegan, this);
@@ -15,34 +29,107 @@ bool StageLayer::init()
 	auto dip = Director::getInstance()->getEventDispatcher();
 	dip->addEventListenerWithSceneGraphPriority(tap, this);
 
-	Sprite* sp = Sprite::create("building_1.png");
-	sp->setPosition(Vec2(designResolutionSize.width*0.5f, designResolutionSize.height*0.5f));
-	addChild(sp);
+	
+	Sprite* comment = Sprite::create("StageBoard_comment.png");
+	comment->setPosition(Vec2(designResolutionSize.width*0.2f, designResolutionSize.height*0.6f));
+	addChild(comment,3);
+
+	desideSP = Sprite::create("umbrella_0.png");
+	desideSP->setPosition(Vec2(XX, YY));
+	addChild(desideSP,1);
+
+	for (int i = 0; i < ALL_STAGE; i++) 
+	{
+		StageItem* sp = StageItem::create(i);
+	RX = (XX - XX)*cos((float)(i*0.2)) -
+		(YY - PY)*sin((float)(i*0.2)) + PX;
+	RY = (XX - XX)*sin((float)(i*0.2)) +
+		(YY - PY)*cos((float)(i*0.2)) + PY;
+
+		sp->setPosition(Vec2(RX,RY));
+		sp->setScale(0.2);
+		addChild(sp,2);
+		Stages.pushBack(sp);
+	}
+	setLetter();
+
+	scheduleUpdate();
 
 	return true;
 };
 
 void StageLayer::update(float delta) 
 {
+		
+	if (moveSpeed > 0)
+		moveSpeed = 0.003f;
+	else if (moveSpeed < 0)
+		moveSpeed = -0.003f;
 
+		
+	for (int i = 0; i < Stages.size(); i++)
+	{
+
+		RX = (Stages.at(i)->getPositionX() - PX)*cos(moveSpeed) -
+			(Stages.at(i)->getPositionY() - PY)*sin(moveSpeed) + PX;
+		RY = (Stages.at(i)->getPositionX() - PX)*sin(moveSpeed) +
+			(Stages.at(i)->getPositionY() - PY)*cos(moveSpeed) + PY;
+
+		Stages.at(i)->setPosition(Vec2(RX, RY));
+
+		if (desideSP->getBoundingBox().intersectsRect(Stages.at(i)->getBoundingBox()))
+		{
+			letter->setString(Stages.at(i)->myData.LETTER);
+			StageNumber = i;
+		}
+
+		//Stages.at(i)->setColor(Color3B(moveSpeed, moveSpeed*10, moveSpeed*12));
+	}
 };
 
-//‰æ–Ê‚ðƒ^ƒbƒ`‚µ‚½Žž‚Ìˆ—
+//ç”»é¢ã‚’ã‚¿ãƒƒãƒã—ãŸæ™‚ã®å‡¦ç†
 bool StageLayer::onTouchBegan(Touch* touch, Event* event)
 {
+	moveSpeed = 0;
 
+	if (touch->getLocation().x >= designResolutionSize.width*0.9f)
+	{
+		Director::getInstance()->replaceScene(TransitionFade::create(2.0f, GameScene::create(StageNumber), Color3B::WHITE));
+	}
+	for (int i = 0; i < Stages.size(); i++)
+	{
+		if (Stages.at(i)->getBoundingBox().containsPoint(touch->getLocation()))
+		{
+			log("get[%d]",i);
+			String* name = String::createWithFormat(Stages.at(i)->myData.LETTER.c_str());
+			letter->setString(name->getCString());
+			Stages.at(i)->myData.LETTER.c_str();
+			isStageTouch = true;
+		}
+	}
 	return true;
 };
 
-//ƒ^ƒbƒ`’†‚Ìˆ—
+//ã‚¿ãƒƒãƒä¸­ã®å‡¦ç†
 void StageLayer::onTouchMoved(Touch* touch, Event* event)
 {
+	if (isStageTouch)
+	{
+		Vec2 delta = touch->getDelta();
+		moveSpeed = delta.x;
+	}
+};
 
+//ã‚¿ãƒƒãƒãŒçµ‚ã‚ã£ãŸæ™‚ã®å‡¦ç†
+void StageLayer::onTouchEnded(Touch *touch, Event *event)
+{
+	isStageTouch = false;
 
 };
 
-//ƒ^ƒbƒ`‚ªI‚í‚Á‚½Žž‚Ìˆ—
-void StageLayer::onTouchEnded(Touch *touch, Event *event)
+void StageLayer::setLetter()
 {
-
+	letter = Label::create("ã“ã“ã«èª¬æ˜Žæ–‡ã¯ã„ã‚‹ã‚“ã ã‘ã©ã­","arial.ttf",40);
+	letter->setPosition(Vec2(designResolutionSize.width*0.2f, designResolutionSize.height*0.85f));
+	addChild(letter,5);
 };
--- Afterimage/Classes/StageLayer.h
+++ Afterimage/Classes/StageLayer.h
@@ -1,7 +1,8 @@
-#ifndef __STAGE_LAYER_H__
+﻿#ifndef __STAGE_LAYER_H__
 #define __STAGE_LAYER_H__
 
 #include "cocos2d.h"
+#include "StageItem.h"
 
 USING_NS_CC;
 
@@ -10,16 +11,27 @@ class StageLayer : public Layer
 public:
 	CREATE_FUNC(StageLayer);
 
+	bool init();
+
 	bool onTouchBegan(Touch* touch, Event* event);
 	void onTouchMoved(Touch* touch, Event* event);
 	void onTouchEnded(Touch* touch, Event* event);
-	bool init();
-
 
 private:
-	
+	int StageNumber;
+	float numbersEye;
+	float moveSpeed;
+	float RX, RY;
+	bool isStageTouch;
+	Sprite* desideSP;
+	Label* letter;
+
+	Vector<StageItem*> Stages;
+
 
 	void update(float delta);
+
+	void setLetter();
 
 
 
